f849bc0dc38be4447178b357da160973680bfc58
GritDim <gritsun.d@gmail.com>
2021-12-01 03:46:42 +0300

    Добавьте файлы проекта.

---
+++ Task7.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine();
+            double S, V;
+            CalcSV(a, out S, out V);
+
+            Console.WriteLine("Площадь поверхности куба {0}, его объем {1}", S,V);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+        static void CalcSV(int a, out double S, out double V)
+        {
+            S = a * a * 6;
+            V = a * a * a;
+            
+        }
+    }
+}
---
+++ Task7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину сторон первого треугольника");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Введите длину сторон второго треугольника");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int c2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            double s1 = CalcSquare(a1, b1, c1);
+            double s2 = CalcSquare(a2, b2, c2);
+
+            if (s1==s2)
+            {
+                Console.WriteLine("Площади треугольников равны");
+            }
+            else
+            {
+                if (s1 > s2)
+                {
+                    Console.WriteLine("Площадь первого треугольника больше");
+                }
+                else
+                {
+                    Console.WriteLine("Площадь второго треугольника больше");
+                }
+            }
+           
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+
+
+        static double CalcSquare(int a, int b, int c)
+        {
+            int p = (a + b + c) / 2;
+            double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return S;
+        }
+
+    }
+}
